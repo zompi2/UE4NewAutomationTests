459cce0229af24bb80644cac34fd44589eb907fe
Damian Nowakowski <dnowakowski.mmnd@gmail.com>
2020-05-01 19:28:29 +0200

    Removed not needed assets to save some github space.

--- AutoTest/Source/AutoTest/AutoTestCharacter.cpp
+++ AutoTest/Source/AutoTest/AutoTestCharacter.cpp
@@ -90,7 +90,7 @@ void AAutoTestCharacter::BeginPlay()
 	Super::BeginPlay();
 
 	//Attach gun mesh component to Skeleton, doing it here because the skeleton is not yet created in the constructor
-	FP_Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
+	//FP_Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
 
 	// Show or hide the two versions of the gun based on whether or not we're using motion controllers.
 	if (bUsingMotionControllers)
